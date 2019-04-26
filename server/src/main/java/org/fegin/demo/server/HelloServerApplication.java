311598aead5e507b8e0741cbd4bafd5c55fddffa
steel <geng_chao_gang@126.com>
2019-04-26 14:41:52 +0800

    update to openfeign

--- server/src/main/java/org/fegin/demo/server/HelloServerApplication.java
+++ server/src/main/java/org/fegin/demo/server/HelloServerApplication.java
@@ -1,13 +1,16 @@
-package demo;
+package org.fegin.demo.server;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Spencer Gibb
@@ -20,9 +23,9 @@ public class HelloServerApplication {
 	DiscoveryClient client;
 
 	@RequestMapping("/")
-	public String hello() {
-		ServiceInstance localInstance = client.getLocalServiceInstance();
-		return "Hello World: "+ localInstance.getServiceId()+":"+localInstance.getHost()+":"+localInstance.getPort();
+	public String hello() throws JsonProcessingException {
+		List<String> localInstance = client.getServices();
+		return "Hello World: "+ new ObjectMapper().writeValueAsString(localInstance);
 	}
 
 	public static void main(String[] args) {
